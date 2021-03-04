260cffead3c51068dbde038be1dbeb072e87e630
QmelZ <qmelzthevoid@gmail.com>
2021-03-04 12:27:15 +0300

    allow write

--- start.bat
+++ start.bat
@@ -1,3 +1,3 @@
 cd /D "%~dp0"
-deno run --allow-net --allow-read main.js
+deno run --allow-net --allow-read --allow-write main.js
 pause
--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 while sleep 1
 do
-    ./deno run --allow-net --allow-read ./main.js
+    ./deno run --allow-net --allow-read --allow-write ./main.js
     if [ $? == 124 ]
     then
         git pull
